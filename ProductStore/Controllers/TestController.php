297aaf9428ec7889ac01c60c882a554f7acd38e8
Sinthu Sathananthan <sinthu@palominosys.com>
2016-08-12 19:19:38 -0400

    number of changes

--- ProductStore/Controllers/TestController.php
+++ ProductStore/Controllers/TestController.php
@@ -11,7 +11,7 @@ use \WebpalCore\Controllers\Core as WebpalCore;
 use Input;
 use \WebpalCore\Source\Services\WebPalResponse;
 
-class DataController extends BaseController
+class TestController extends BaseController
 {
   protected $wp;
   public function __construct() {
@@ -30,8 +30,24 @@ class DataController extends BaseController
   * queries a store by certain criteria and renders results as a view
   */
   public function search($storeid) {
+    // storeid - the id of the product-store node to query
+    // q - the query from the request, used to perform a partial match on a product's title
+    
+    
+    
+    $storeid = 'wpPrNyFKIOawa9a1';
+    $cond = "contains(title, \"laptop\")";
+    
+    $xquery = "//product-store[@id='$storeid']//product[1]";
+    
+    $params = [];
+    //$params['mode'] = 'search';
+    //return $xquery;
+    return WebPalResponse::view($xquery, array(), 200, array(), $params);
+    
     $q = Input::get('q');
-    $cond = "contains(title, '$q')";
+    //$cond = "contains(title, '$q')";
+    $cond = "contains(title, 'gloves')";
     // $cond = "1";
     $xquery = "//product-store[@id='$storeid']/products/product[$cond]";
     $params['mode'] = 'search';
---
+++ ProductStore/Resources/js/product-list.js
@@ -0,0 +1,24 @@
+function product_list_add_cart(itemCode) {
+  alert ("Added " + itemCode + "to cart!");
+} 
+
+
+$(document).ready(function(){
+
+  // SmartMenus jQuery init
+  // $('#productMenu').smartmenus({
+  //   mainMenuSubOffsetX: -1,
+  //   subMenusSubOffsetX: 10,
+  //   subMenusSubOffsetY: 0
+  // });
+  $( "span.sub-arrow" ).html( "" );
+
+
+  // Instantiate EasyZoom plugin
+  var $easyzoom = $('.easyzoom').easyZoom();
+
+  // Get the instance API
+  var api = $easyzoom.data('easyZoom');
+  
+
+});    
